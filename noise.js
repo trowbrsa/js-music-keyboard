f046778e0cff5f1b033cea803cba3c19af49945d
Sarah <trowbrsa@gmail.com>
2016-01-27 14:51:25 -0800

    First couple ntoes on keyboard make sound

--- noise.js
+++ noise.js
@@ -1,3 +1,18 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+  $(".instrument .note.c").click(function () {
+    // set timeout so that the note can be played repeatedly without pause
+    $("#audio_embeds #cAudio").get(0).currentTime=0;
+    // play the sound
+    $("#audio_embeds #cAudio").get(0).play();
+  });
+
+  $(".instrument .note.d").click(function () {
+    $("#audio_embeds #dAudio").get(0).currentTime=0;
+    $("#audio_embeds #dAudio").get(0).play();
+  });
+
+  $(".instrument .note.e").click(function () {
+    $("#audio_embeds #eAudio").get(0).play();
+  });
+
 });

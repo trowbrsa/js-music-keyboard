b69c0f48bb7b42a32ab175c409fe28cec1172925
Sarah <trowbrsa@gmail.com>
2016-01-28 10:28:56 -0800

    Dried up code

--- noise.js
+++ noise.js
@@ -1,33 +1,53 @@
 $(document).ready(function() {
-  $(".instrument .note.c").click(function () {
-    // set timeout so that the note can be played repeatedly without pause
-    $("#audio_embeds #cAudio").get(0).currentTime=0;
-    // play the sound
-    $("#audio_embeds #cAudio").get(0).play();
-  });
+  var playNote = function(selector) {
+    selector.get(0).currentTime=0;
+    selector.get(0).play();
+  };
 
-  $(".d").click(function () {
-    $("#dAudio").get(0).currentTime=0;
-    $("#dAudio").get(0).play();
-  });
+  var notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  // var keys = [look up decimal values for keys]
 
-  $(".e").click(function () {
-    $("#eAudio").get(0).play();
-  });
+  notes.forEach( function(note) {
+    $('.' + note).click( function() {
+      playNote($('#' + note + "Audio"));
+    });
 
-  $(".f").click(function () {
-    $("#fAudio").get(0).play();
-  });
+  keys.forEach( function(key) {
+    $('.' + key).keypress( function() {
+      playNote($('#' + key + "Audio"));
+    });
 
-  $(".g").click(function () {
-    $("#gAudio").get(0).play();
   });
-
-  $(".a").click(function () {
-    $("#aAudio").get(0).play();
-  });
-
-  $(".b").click(function () {
-    $("#bAudio").get(0).play();
-  });
+// // previous code, the DRY version is above
+//   $(".instrument .note.c").click(function () {
+//     // set timeout so that the note can be played repeatedly without pause
+//     $("#audio_embeds #cAudio").get(0).currentTime=0;
+//     // play the sound
+//     $("#audio_embeds #cAudio").get(0).play();
+//   });
+  //
+  // $(".d").click(function () {
+  //   $("#dAudio").get(0).currentTime=0;
+  //   $("#dAudio").get(0).play();
+  // });
+  //
+  // $(".e").click(function () {
+  //   $("#eAudio").get(0).play();
+  // });
+  //
+  // $(".f").click(function () {
+  //   $("#fAudio").get(0).play();
+  // });
+  //
+  // $(".g").click(function () {
+  //   $("#gAudio").get(0).play();
+  // });
+  //
+  // $(".a").click(function () {
+  //   $("#aAudio").get(0).play();
+  // });
+  //
+  // $(".b").click(function () {
+  //   $("#bAudio").get(0).play();
+  // });
 });
